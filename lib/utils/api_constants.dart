438aaadf777b641b11f7287f8f89a566480c0c22
Himal Patel <hnp.2792@gmail.com>
2026-05-16 15:17:19 +0530

    fix icon and splash screen issue

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -4,7 +4,10 @@ class ApiConstants {
   // Use your machine's IP address for physical devices
   //static const String authBaseUrl = 'http://192.168.27.58:3001/api';
   //static const String kudoUrl = 'http://192.168.27.57:3003';
-  static const String qrLink = 'https://kudo.responscity.com';
-  static const String kudoUrl = 'https://api.prod-kudo.responscity.com';
-  static const String authBaseUrl = 'https://api.prod-stud.responscity.com/api';
+  // static const String qrLink = 'https://kudo.responscity.com';
+  // static const String kudoUrl = 'https://api.prod-kudo.responscity.com';
+  // static const String authBaseUrl = 'https://api.prod-stud.responscity.com/api';
+  static const String qrLink = 'https://kudo-dev.responscity.com';
+  static const String kudoUrl = 'https://api.dev-kudo.responscity.com';
+  static const String authBaseUrl = 'https://api.dev-stud.responscity.com/api';
 }
